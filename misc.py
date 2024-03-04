1c78e91d667e72635d22829a813c8a1b65145fdd
Antoine Grenier <antoine.grenier@tuni.fi>
2024-03-04 10:49:44 +0200

    Added quantization of signals

---
+++ misc.py
@@ -0,0 +1,86 @@
+
+import numpy as np
+import scipy as sp
+
+import sys
+sys.path.append("/mnt/c/Users/vmangr/Documents/Code/sydr/")
+from sydr.signal.gnsssignal import GenerateGPSGoldCode
+from sydr.utils.constants import GPS_L1CA_CODE_FREQ
+
+# =============================================================================
+
+def getPowerdB(value_linear):
+    return 10 * np.log10(value_linear)
+
+# =============================================================================
+
+def getPowerLinear(value_dB):
+    return 10 ** (value_dB/10)
+
+# =============================================================================
+
+def generateGPSL1CA(prn=1, samplingFrequency=None):
+    # Generate PRN code
+    prn_code = GenerateGPSGoldCode(prn=prn, samplingFrequency=samplingFrequency)
+
+    # Generate doppler 
+    # TODO 
+
+    return prn_code
+
+# =============================================================================
+
+def addWhiteNoise(signal, sigma):
+
+    noise = np.random.normal(0, scale=sigma, size=len(signal))
+    
+    #signal_out = signal + noise * sigma
+    signal_out = signal + (noise - np.mean(noise))/np.std(noise)*sigma # From Simona's file
+    #signal_out = signal + noise
+
+    return signal_out
+
+# =============================================================================
+
+def getSigmaFromCN0(signal_power_dB, cn0_target_dB, signal_bw):
+    
+    snr_target_dB = cn0_target_dB - getPowerdB(signal_bw) # TODO Should we add the bandwidth of the sampling frequency as well? 
+
+    noise_power = getPowerLinear(signal_power_dB) / getPowerLinear(snr_target_dB)
+    sigma = np.sqrt(noise_power)
+
+    return sigma
+
+# =============================================================================
+
+def quantize(signal, n_bits):
+
+    max_val = np.max(np.abs(signal))
+
+    num_levels = 2**n_bits 
+
+    bins = np.linspace(-max_val, max_val, num_levels)
+    centers = (bins[1:]+bins[:-1])/2
+
+    signal_quantized = np.digitize(signal, centers) - (num_levels // 2) 
+
+    return signal_quantized
+
+
+# =============================================================================
+
+def correlate(signal_1, signal_2, method='scipy', normalize=False):
+
+    # Normalization
+    # From https://stackoverflow.com/questions/53436231/normalized-cross-correlation-in-python
+    if normalize:
+        signal_1 = (signal_1 - np.mean(signal_1)) / (np.std(signal_1) * len(signal_1))
+        signal_2 = (signal_2 - np.mean(signal_2)) / (np.std(signal_2))
+
+    # Swith correlation method
+    match method:
+        case 'scipy':
+            corr = sp.signal.correlate(signal_1, signal_2, mode='full')
+            lags = sp.signal.correlation_lags(len(signal_1), len(signal_2), mode="full")
+
+    return corr, lags
---
+++ plot.py
@@ -0,0 +1,78 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+COLORS = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+
+# =============================================================================
+
+def plotSignals(signals, sampling_frequency, code_frequency, title):
+
+    fig = plt.figure(figsize=(12, 2.5*len(signals)), layout="constrained")
+    spec = fig.add_gridspec(len(signals), 3)
+    fig.suptitle(title, size='x-large', fontweight='bold')
+
+    colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+    lim_psd = code_frequency*2
+    
+    axs = []
+    idx = 0
+    for name, signal in signals.items():
+        ax_plot = fig.add_subplot(spec[idx, 0:2])
+        ax_plot.plot(signal, color=COLORS[idx])
+        ax_plot.set_title(name)
+        ax_plot.set_ylabel('Amplitude [unitless]')
+        ax_plot.set_xlabel('Samples')
+
+        ax_psd = fig.add_subplot(spec[idx, 2:])
+        ax_psd.psd(signal, Fs=sampling_frequency, sides='twosided', color=COLORS[idx])
+        ax_psd.set_ylabel("PSD [dB/Hz]")
+        ax_psd.set_xlim(-lim_psd, lim_psd)
+
+        axs.append(ax_plot)
+        axs.append(ax_psd)
+        idx += 1
+
+    for ax in axs:
+        ax.grid(visible=True)
+    
+    plt.show()
+
+    return
+
+# =============================================================================
+
+def plotCorrelation(correlations, sampling_frequency, code_frequency, title):
+
+    fig = plt.figure(figsize=(12, 4), layout="constrained")
+    spec = fig.add_gridspec(1, 3)
+    fig.suptitle(title, size='x-large', fontweight='bold')
+
+    samplesPerChip = round(sampling_frequency / code_frequency)
+    lim_corr_large = 100
+    lim_corr_small = 5
+
+    idx = 0 
+    ax_large = fig.add_subplot(spec[idx:, 0:2])
+    ax_small = fig.add_subplot(spec[idx:, 2:])
+    for name, corr in correlations.items():
+        corr_values, corr_lags = corr
+        ax_large.plot(corr_lags/samplesPerChip, corr_values / np.max(corr_values), color=COLORS[idx], label=name)
+        ax_large.set_xlim(-lim_corr_large, lim_corr_large)
+        ax_large.set_title("Correlation")
+        ax_large.set_xlabel('Chips')
+
+        ax_small.plot(corr_lags/samplesPerChip, corr_values / np.max(corr_values), color=COLORS[idx], label=name)
+        ax_small.set_xlim(-lim_corr_small, lim_corr_small)
+        ax_small.set_xlabel('Chips')
+
+        idx += 1
+
+    axs = [ax_large, ax_small]
+    for ax in axs:
+        ax.grid(visible=True)
+        ax.legend()
+    
+    plt.show()
+
+    return
